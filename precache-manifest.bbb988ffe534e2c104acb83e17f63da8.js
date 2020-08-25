cb50edb1da0bcf9a71348c8ca904803b272df2b4
onurkaraoglan <okaraoglan91@gmail.com>
2020-08-25 21:42:28 +0300

    Updates

---
+++ precache-manifest.bbb988ffe534e2c104acb83e17f63da8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54dab52e8f8f1f569edef41f62d8306f",
+    "url": "/react-to-do-list/index.html"
+  },
+  {
+    "revision": "7272566bfce4e10de72f",
+    "url": "/react-to-do-list/static/css/main.34fec8ec.chunk.css"
+  },
+  {
+    "revision": "57eb1d29c319c42dc924",
+    "url": "/react-to-do-list/static/js/2.0249f3c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-to-do-list/static/js/2.0249f3c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7272566bfce4e10de72f",
+    "url": "/react-to-do-list/static/js/main.95af2713.chunk.js"
+  },
+  {
+    "revision": "8c3d49c384a4bcf49218",
+    "url": "/react-to-do-list/static/js/runtime-main.f9d0e86f.js"
+  }
+]);
